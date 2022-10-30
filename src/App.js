7691ccf8da9adbd585f040798f021b3336a4a11a
Aung Yang <aungkyang622@gmail.com>
2022-10-30 14:28:11 -0700

    redux not working, attempted using redux toolkit

--- src/App.js
+++ src/App.js
@@ -2,32 +2,14 @@ import './App.css';
 import React from 'react';
 import Note from './components/Note';
 // import Home from './pages/home';
+
+
+
 import Auth from './components/auth';
 import { Breadcrumb, Layout, Menu, Button } from 'antd';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
 const { Header, Content, Footer, Sider } = Layout;
 
-// TODO: DELETE THIS
-// const items1 = ['1', '2', '3'].map((key) => ({
-//   key,
-//   label: `nav ${key}`,
-// }));
-// TODO: DELETE THIS
-// const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-//   const key = String(index + 1);
-//   return {
-//     key: `sub${key}`,
-//     icon: React.createElement(icon),
-//     label: `subnav ${key}`,
-//     children: new Array(4).fill(null).map((_, j) => {
-//       const subKey = index * 4 + j + 1;
-//       return {
-//         key: subKey,
-//         label: `option${subKey}`,
-//       };
-//     }),
-//   };
-// });
 
 const App = () => {
   return (
--- src/components/auth.js
+++ src/components/auth.js
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { Button, Form, Input } from 'antd';
-import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { collection, getDocs } from "firebase/firestore"; 
 import { db } from "../firebase" //imported from firebase.js
 import { auth, createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
   signOut } from "../firebase";
+
+  // import for redux stuff
+  import {useDispatch, useSelector} from 'react-redux'
+  import { signin, signout } from '../redux/user';
+
 
 const Auth = () => {
   const [registerEmail, setRegisterEmail] = useState("");
@@ -13,46 +18,40 @@ const Auth = () => {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
 
-
-  const [user, setUser] = useState({});
-
-  const docRef = collection(db, "messages");
+    // useSelector accesses the store's state
+    const {user}  = useSelector(state => state.user);
+     //useDispatch allows you to call actions
+    const dispatch = useDispatch()
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        console.log("user: ", user);
-        // ...
+    onAuthStateChanged(auth, (AuthUser) => {
+      if (AuthUser) {
+        // console.log("AuthUser: ", AuthUser);
+
+         dispatch(signin({
+          user: AuthUser.uid,
+         }));
+
       } else {
-        // User is signed out
-        // ...
-        
+        dispatch(signout());
 
       }
     });
   })
 
 
-
-  // read from firestore
-  const read = async (e) => {
-    e.preventDefault();
-    const querySnapshot = await getDocs(collection(db, "messages"));
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-        return (doc.data().message);
-    });
-}
-
   const register = async () => {
     try {
-      const user = await createUserWithEmailAndPassword(
+      await createUserWithEmailAndPassword(
         auth,
         registerEmail,
         registerPassword
       );
+
+      // dispatch(signin({
+      //   user: AuthUser.uid,
+      // }));
+
     } catch (error) {
       console.log(error.message);
     }
@@ -60,11 +59,16 @@ const Auth = () => {
 
   const login = async () => {
     try {
-      const user = await signInWithEmailAndPassword(
+      await signInWithEmailAndPassword(
         auth,
         loginEmail,
         loginPassword
       );
+
+      // dispatch(signin({
+      //   user: AuthUser.uid,
+      // }));
+
       console.log("SIGN IN IS SUCCESSFUL!!!!");
     } catch (error) {
       console.log(error.message);
@@ -74,12 +78,14 @@ const Auth = () => {
   const logout = async () => {
     await signOut(auth);
     console.log("signed out!");
-    console.log(user);
-
+    
+    // dispatch to store with logout action
+    dispatch(signout());
   };
 
   return (
     <div>
+      USER: {user}
     <Form
       labelCol={{
         span: 8,
@@ -136,7 +142,7 @@ const Auth = () => {
         <Button type="primary" htmlType="submit">REGISTER</Button>
       </Form.Item>
     
-    {/* end of form */}
+    {/* end of register form */}
     </Form> 
 
     <Form
@@ -192,63 +198,16 @@ const Auth = () => {
         <Button type="primary" htmlType="submit">LOGIN</Button>
       </Form.Item>
     
-    {/* end of form */}
+    {/* end of login form */}
     </Form> 
 
     <Button type="primary" htmlType="submit" onClick={logout}>LOGOUT</Button>
-
-    <form onSubmit={read}>
-        <button type="submit">read!</button>
-    </form>
-
+    
 
 
     </div>
   )
-  // return (
-  //   <div className="App"></div>
-  //     {/* <div>
-  //       <h3> Register User </h3>
-  //       <input
-  //         placeholder="Email..."
-  //         onChange={(event) => {
-  //           setRegisterEmail(event.target.value);
-  //         }}
-  //       />
-  //       <input
-  //         placeholder="Password..."
-  //         onChange={(event) => {
-  //           setRegisterPassword(event.target.value);
-  //         }}
-  //       />
-
-  //       <button onClick={register}> Create User</button>
-  //     </div>
-
-  //     <div>
-  //       <h3> Login </h3>
-  //       <input
-  //         placeholder="Email..."
-  //         onChange={(event) => {
-  //           setLoginEmail(event.target.value);
-  //         }}
-  //       />
-  //       <input
-  //         placeholder="Password..."
-  //         onChange={(event) => {
-  //           setLoginPassword(event.target.value);
-  //         }}
-  //       />
-
-  //       <button onClick={login}> Login</button>
-  //     </div>
-
-  //     <h4> User Logged In: </h4>
-  //     {user?.email}
-
-  //     <button onClick={logout}> Sign Out </button>
-  //   </div> */}
-  // );
+  
 }
 
 export default Auth;
